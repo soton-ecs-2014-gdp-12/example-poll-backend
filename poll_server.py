0f602cbbc50acfecd57a351db6e549b5b3182aad
Christopher Hewett <chewett@hotmail.co.uk>
2014-10-20 12:16:31 +0000

    Added a basic decorator for the header adding

--- poll_server.py
+++ poll_server.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from functools import wraps
+from flask import Flask, make_response
 app = Flask(__name__)
+
+
+def add_cors(f):
+    @wraps(f)
+    def inner(*args, **kwargs):
+        response = f(*args, **kwargs)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+    return inner
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/cors_test')
+@add_cors
 def cors_test():
-    return 'cors response'
+    resp = make_response()
+    resp.data = 'cors response'
+    return resp
 
 if __name__ == '__main__':
     app.run()
