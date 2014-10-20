4455af9c5edac2169a816cdd812aa6b6ec5d94ce
Christopher Hewett <chewett@hotmail.co.uk>
2014-10-20 11:47:16 +0000

    Added a cors testing path

--- poll_server.py
+++ poll_server.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/cors_test')
+def cors_test():
+    return 'cors response'
+
 if __name__ == '__main__':
     app.run()
