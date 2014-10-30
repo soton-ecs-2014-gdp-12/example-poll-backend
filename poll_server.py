43885aa22e7e07d8fbc93a65662661ba0e519279
Samuel Bennett <sb21g11@soton.ac.uk>
2014-10-30 14:19:14 +0000

    Fix CORS and add GET results endpoint

--- poll_server.py
+++ poll_server.py
@@ -6,13 +6,47 @@ app = Flask(__name__)
 
 db_name = "votes.db"
 
-def add_cors(f):
-    @wraps(f)
-    def inner(*args, **kwargs):
-        response = f(*args, **kwargs)
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        return response
-    return inner
+# CORS from http://coalkids.github.io/flask-cors.html
+
+@app.before_request
+def option_autoreply():
+    """ Always reply 200 on OPTIONS request """
+
+    if request.method == 'OPTIONS':
+        resp = app.make_default_options_response()
+
+        headers = None
+        if 'ACCESS_CONTROL_REQUEST_HEADERS' in request.headers:
+            headers = request.headers['ACCESS_CONTROL_REQUEST_HEADERS']
+
+        h = resp.headers
+
+        # Allow the origin which made the XHR
+        h['Access-Control-Allow-Origin'] = request.headers['Origin']
+        # Allow the actual method
+        h['Access-Control-Allow-Methods'] = request.headers['Access-Control-Request-Method']
+        # Allow for 10 seconds
+        h['Access-Control-Max-Age'] = "10"
+
+        # We also keep current headers
+        if headers is not None:
+            h['Access-Control-Allow-Headers'] = headers
+
+        return resp
+
+
+@app.after_request
+def set_allow_origin(resp):
+    """ Set origin for GET, POST, PUT, DELETE requests """
+
+    h = resp.headers
+
+    # Allow crossdomain for other HTTP Verbs
+    if request.method != 'OPTIONS' and 'Origin' in request.headers:
+        h['Access-Control-Allow-Origin'] = request.headers['Origin']
+
+
+    return resp
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -26,14 +60,12 @@ def hello_world():
     return 'Hello World!'
 
 @app.route('/cors_test')
-@add_cors
 def cors_test():
     resp = make_response()
     resp.data = 'cors response'
     return resp
 
 @app.route('/setup')
-@add_cors
 def setup():
     print('setup time')
     resp = make_response()
@@ -49,15 +81,16 @@ def setup():
     resp.data = 'db created'
     return resp
 
-@app.route('/results')
-@add_cors
-def results():
-    resp = make_response()
-    resp.data = 'results'
-    return resp
+@app.route('/results/<annotationId>/<questionId>', methods=["GET"])
+def results(annotationId, questionId):
+
+    db = get_db()
+    cur = db.cursor()
+
+    cur.execute('SELECT result, count(*) FROM votes WHERE questionID = ? AND annotationID = ? GROUP BY result', (questionId, annotationId))
+    return jsonify(cur.fetchall());
 
 @app.route('/vote', methods=["POST"])
-@add_cors
 def vote():
     vote = request.get_json(force=True)
 
