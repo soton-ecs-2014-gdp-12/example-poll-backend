e6a50ddaf67a15a704788811ba72ad92ecf08633
Christopher Hewett <chewett@hotmail.co.uk>
2014-11-26 13:57:12 +0000

    Added a very basic test to ensure routing is valid

---
+++ test_routing.py
@@ -0,0 +1,21 @@
+import os
+import poll_server
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        poll_server.app.config['TESTING'] = True
+        self.app = poll_server.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_routing_maintest_page_exists(self):
+        response = self.app.get('/')
+        assert 'Hello World!' in response.data
+
+
+if __name__ == '__main__':
+    unittest.main()
